0145c3c6c0007867d539442ab67a87d0629f6c89
Maks-oss <mpavlov198@gmail.com>
2020-05-19 16:21:35 +0300

    fix

--- lab5/lab5.cpp
+++ lab5/lab5.cpp
@@ -3,7 +3,7 @@
 #include <vector>
 #include <string>
 using namespace std;
-#define MAX 129269
+#define MAX 129266
 
 struct place
 {
@@ -18,7 +18,7 @@ place* readplace(ifstream& file)
 {
 	place* coor = new place[MAX];
 	string s;
-	setlocale(LC_ALL, "RUSSIAN"); //короче это штука очень важная
+	setlocale(LC_ALL, "ukr"); //короче это штука очень важная
 	for (int i = 0; i < MAX; i++)
 	{
 		getline(file, s, ';');
@@ -40,7 +40,6 @@ place* readplace(ifstream& file)
 
 int main()
 {
-	system("chcp 1251");
 	ifstream file("ukraine_poi.csv");
 	place* coor = readplace(file);
 }
