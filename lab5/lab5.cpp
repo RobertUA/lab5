df9018a8ce92af56505a55de0d3752c395fff080
Роберт Шевцов <55320224+RobertUA@users.noreply.github.com>
2020-05-19 11:02:26 +0300

    out

--- lab5/lab5.cpp
+++ lab5/lab5.cpp
@@ -1,7 +1,7 @@
 ﻿#include <iostream>
-#include<fstream>
-#include<vector>
-#include<string>
+#include <fstream>
+#include <vector>
+#include <string>
 using namespace std;
 #define MAX 129269
 
@@ -18,7 +18,7 @@ place* readplace(ifstream& file)
 {
 	place* coor = new place[MAX];
 	string s;
-	setlocale(LC_ALL, "ukr"); //короче это штука очень важная
+	setlocale(LC_ALL, "RUSSIAN"); //короче это штука очень важная
 	for (int i = 0; i < MAX; i++)
 	{
 		getline(file, s, ';');
@@ -33,12 +33,14 @@ place* readplace(ifstream& file)
 		coor[i].name = s;
 		getline(file, s, ';');
 		coor[i].address = s;
+		cout << "[" << i << "] " << coor[i].width << " " << coor[i].height << " " << coor[i].type << " " << coor[i].undertype << " " << coor[i].name << " " << coor[i].address << "\n";
 	}
 	return coor;
 }
 
 int main()
 {
-	ifstream file("d:\\ukraine_poi.csv");
+	system("chcp 1251");
+	ifstream file("ukraine_poi.csv");
 	place* coor = readplace(file);
 }
