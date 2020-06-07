eb3d4b861f18c22a66cc28790731a572398b4e37
Роберт Шевцов <55320224+RobertUA@users.noreply.github.com>
2020-06-07 13:26:31 +0300

    Dist

--- lab5/lab5.cpp
+++ lab5/lab5.cpp
@@ -2,8 +2,14 @@
 #include <fstream>
 #include <vector>
 #include <string>
+#include <cmath>
+
 using namespace std;
 #define MAX 129266
+#define PI 3.1415926535
+#define R 6371
+
+double Distance(double weight1, double long1, double weight2, double long2);
 
 struct place
 {
@@ -34,8 +40,9 @@ place* readplace(ifstream& file)
 		coor[i].name = s;
 		getline(file, s, ';');
 		coor[i].address = s;
-		cout << "[" << i << "] " << coor[i].width << " " << coor[i].height << " " << coor[i].type << " " << coor[i].undertype << " " << coor[i].name << " " << coor[i].address << "\n";
+		//cout << "[" << i << "] " << coor[i].width << " " << coor[i].height << " " << coor[i].type << " " << coor[i].undertype << " " << coor[i].name << " " << coor[i].address << "\n";
 	}
+	cout << endl << fixed << Distance(49, 33, 49.35, 34.32);
 	return coor;
 }
 
@@ -43,4 +50,16 @@ int main()
 {
 	ifstream file("ukraine_poi.csv");
 	place* coor = readplace(file);
+}
+
+double Distance(double weight1, double long1, double weight2, double long2)
+{
+	weight1 = (weight1 / 180) * PI;
+	weight2 = (weight2 / 180) * PI;
+	long1 = (long1 / 180) * PI;
+	long2 = (long2 / 180) * PI;
+	double AvgWeight = (weight1 + weight2) / 2;
+	return R*sqrt((weight2-weight1)*(weight2-weight1) 
+		+ (cos(AvgWeight)*(long2-long1)) 
+		* (cos(AvgWeight)*(long2-long1)));
 }
